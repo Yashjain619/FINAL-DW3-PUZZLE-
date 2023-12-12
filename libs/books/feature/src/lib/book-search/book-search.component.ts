7e25c7809b4ce7d4109274ddb3634b8bf866a761
yashj619@gmail.com <Yash.Jain3@T-Mobile.com>
2023-12-12 21:20:05 +0530

    Task 2 - instant search

--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -1,26 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy,Component,OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import {
   addToReadingList,
   clearSearch,
   getAllBooks,
+  getBooksError,
+  getBooksLoaded,
   ReadingListBook,
   searchBooks
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
 import { Book } from '@tmo/shared/models';
+import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
+import { Subject, Observable } from 'rxjs';
 
 @Component({
   selector: 'tmo-book-search',
   templateUrl: './book-search.component.html',
-  styleUrls: ['./book-search.component.scss']
+  styleUrls: ['./book-search.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class BookSearchComponent implements OnInit {
-  books: ReadingListBook[];
+export class BookSearchComponent implements OnInit, OnDestroy {
+  books$: Observable<ReadingListBook[]> = this.store.select(getAllBooks);
+  private unsubscribeObservable$: Subject<void> = new Subject();
 
   searchForm = this.fb.group({
     term: ''
   });
+  getBooksLoaded$: Observable<boolean> = this.store.select(getBooksLoaded);
+  getBooksError$: Observable<string> = this.store.select(getBooksError);
+  ERROR_TEXT = 'No Result Found !!';
+  SPINNER_DIAMETER = 40;
+  SPINNER_WIDTH = 2;
 
   constructor(
     private readonly store: Store,
@@ -32,31 +43,30 @@ export class BookSearchComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.store.select(getAllBooks).subscribe(books => {
-      this.books = books;
-    });
+    this.searchForm.get('term').valueChanges
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        takeUntil(this.unsubscribeObservable$))
+      .subscribe((searchTerm) => {
+        if (searchTerm) {
+          this.store.dispatch(searchBooks({ term: searchTerm }));
+        } else {
+          this.store.dispatch(clearSearch());
+        }
+      });
   }
 
-  formatDate(date: void | string) {
-    return date
-      ? new Intl.DateTimeFormat('en-US').format(new Date(date))
-      : undefined;
+  ngOnDestroy(): void {
+    this.unsubscribeObservable$.next();
+    this.unsubscribeObservable$.complete();
   }
 
-  addBookToReadingList(book: Book) {
+  addBookToReadingList(book: Book): void {
     this.store.dispatch(addToReadingList({ book }));
   }
 
-  searchExample() {
+  searchExample(): void {
     this.searchForm.controls.term.setValue('javascript');
-    this.searchBooks();
-  }
-
-  searchBooks() {
-    if (this.searchForm.value.term) {
-      this.store.dispatch(searchBooks({ term: this.searchTerm }));
-    } else {
-      this.store.dispatch(clearSearch());
-    }
   }
 }
